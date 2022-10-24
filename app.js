4322a0a775a73882807068f3257d83c377a2f097
dlawhdgus <212whdgus@gmail.com>
2022-10-24 16:34:50 +0900

    change message

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const { Client, GatewayIntentBits, EmbedBuilder } = require('discord.js')
+const { Client, GatewayIntentBits } = require('discord.js')
 const config = require('./config.json')
 const client = new Client({
     intents: [
@@ -14,35 +14,34 @@ client.once('ready', () => {
     console.log('ready')
 })
 
+
 client.on('messageCreate', msg => {
-    const content = msg.content
-    const CommandFilter = content.split('!')[1]
+    const content = msg.content.split('!')[1]
+    if(!content) return
     const send = (message) => {
-        msg.channel.send(`${message}`)
+        msg.channel.send(message)
     }
-    if (CommandFilter) {
-        switch (CommandFilter) {
-            case 'help':
-                send(`!1 ~ 5까지 숫자를 입력해 주세요`)
-                break
-            case '1':
-                send('hello')
-                break
-            case '2':
-                send('안녕하세요')
-                break
-            case '3':
-                send('hola')
-                break
-            case '4':
-                send('bonjour')
-                break
-            case '5':
-                send('你好')
-                break
-            default:
-                send('wrong command')
-        }
+    switch (content) {
+        case 'help':
+            send(`1 ~ 5까지 숫자를 입력해 주세요`)
+            break
+        case '1':
+            send('hello')
+            break
+        case '2':
+            send('안녕하세요')
+            break
+        case '3':
+            send('hola')
+            break
+        case '4':
+            send('bonjour')
+            break
+        case '5':
+            send('你好')
+            break
+        default:
+            send('wrong command')
     }
 })
 
